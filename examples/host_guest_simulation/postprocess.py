e993ec09a1e92eac3f8b328b34a78051c1968440
David Slochower <slochower@gmail.com>
2017-12-22 14:51:00 -0800

    Mapping of residue number working. Mapping of residue name seems to work, but is not written to the output mol2 file.

--- examples/host_guest_simulation/postprocess.py
+++ examples/host_guest_simulation/postprocess.py
@@ -5,14 +5,5 @@ Postprocess the `mol2` file to correct residue name and numbering.
 """
 
 
-def rename_atoms_and_residues(mapping, mol2):
+def rename_residue(mapping, mol2):
     pass
-    # For mapping X --> Y
-    # Find the line corresponding to atom X in reference
-    # Find the line corresponding to atom Y in target
-    # Copy atom name
-    # Copy residue number
-
-
-    # What to do about atom index...
-    # Keep atom indices the same...
--- examples/host_guest_simulation/utils.py
+++ examples/host_guest_simulation/utils.py
@@ -5,6 +5,7 @@ Provides helper functions for converting between AMBER files and OpenEye OEMols.
 """
 
 import numpy as np
+import subprocess as sp
 from openeye.oechem import *
 from openforcefield.typing.engines.smirnoff import *
 from networkx.algorithms import isomorphism
@@ -25,10 +26,14 @@ def process_and_convert(smiles, reference_file, name):
 
     """
     smiles_mol = process_smiles(smiles, name=name, add_hydrogens=True)
-    reference_mol = load_mol2(reference_file, name=name, add_tripos=True)
+    reference_mol = load_mol2(reference_file, name=name, add_tripos=False)
     reference_to_target_mapping = atom_mapping(reference_mol, smiles_mol)
+    smiles_mol = remap_names(reference_to_target_mapping, reference_mol, smiles_mol)
     smiles_mol = remap_charges(reference_to_target_mapping, reference_mol, smiles_mol)
-    smiles_mol = remap_type(reference_to_target_mapping, reference_mol, smiles_mol)
+    resname = parse_residue_name(reference_file)
+    smiles_mol = remap_residues(reference_to_target_mapping, reference_mol, smiles_mol,
+                                resname=resname)
+    # smiles_mol = remap_type(reference_to_target_mapping, reference_mol, smiles_mol)
     smiles_mol = remap_coordinates(reference_to_target_mapping, reference_mol, smiles_mol)
     print('Either use the returned `OEMol()` or save as `mol2` to continue...')
     return smiles_mol
@@ -46,12 +51,20 @@ def load_mol2(filename, name=None, add_tripos=True):
         return mol
 
 
-def save_mol2(filename, mol, standard=True):
+def save_mol2(filename, mol, standard=True, add_tripos=True):
+    # I am not sure why, but I cannot get this to write the residue name.
     ofs = oemolostream()
     if not ofs.open(filename):
         print(f'Unable to open {filename} for writing...')
+    if add_tripos:
+        OETriposAtomNames(mol)
     if standard:
         OEWriteMolecule(ofs, mol)
+    else:
+        # ofs.SetFormat(OEFormat_MOL2H)
+        flavor = OEOFlavor_Generic_Default | OEOFlavor_MOL2H_Default | OEOFlavor_MOL2H_AtomTypeNames
+        ofs.SetFlavor(OEFormat_MOL2H, flavor)
+        OEWriteMol2File(ofs, mol)
     ofs.close()
 
 
@@ -85,6 +98,7 @@ def atom_mapping(reference, target):
     reference_to_target_mapping = dict()
     graph_matcher = isomorphism.GraphMatcher(reference_graph, target_graph)
     if graph_matcher.is_isomorphic():
+        print('Determining mapping...')
         print('Reference → Target')
         for (reference_atom, target_atom) in graph_matcher.mapping.items():
             reference_to_target_mapping[reference_atom] = target_atom
@@ -108,7 +122,7 @@ def create_graph(topology):
 
 
 def remap_charges(reference_to_target_mapping, reference_mol, target_mol):
-    print('Remap charges...')
+    print('Remapping charges...')
     print('Existing → New')
     assert reference_mol.GetMaxAtomIdx() == target_mol.GetMaxAtomIdx()
     for (reference_atom, target_atom) in reference_to_target_mapping.items():
@@ -125,8 +139,68 @@ def remap_charges(reference_to_target_mapping, reference_mol, target_mol):
     return target_mol
 
 
+def remap_names(reference_to_target_mapping, reference_mol, target_mol):
+    print('Remapping atom names...')
+    print('Reference → Target')
+    assert reference_mol.GetMaxAtomIdx() == target_mol.GetMaxAtomIdx()
+    for (reference_atom, target_atom) in reference_to_target_mapping.items():
+        reference = reference_mol.GetAtom(OEHasAtomIdx(reference_atom))
+        reference_name = reference.GetName()
+
+        target = target_mol.GetAtom(OEHasAtomIdx(target_atom))
+        target_name = target.GetName()
+        target.SetName(reference_name)
+        print(f'{reference_name:4} → {target_name:4}')
+    return target_mol
+
+
+def parse_residue_name(input_mol2, path='./'):
+    p = sp.Popen(['awk', '{print $8}', input_mol2], cwd=path, stdout=sp.PIPE)
+    for line in p.stdout:
+        if line.decode("utf-8").split() != []:
+            name = line.decode("utf-8").split()[0]
+            print(f'Found residue name = {name}')
+            return name
+        else:
+            pass
+    return None
+
+
+def remap_residues(reference_to_target_mapping, reference_mol, target_mol, resname=None):
+    print('Remapping residue names and numbers...')
+    print('Existing → New')
+    assert reference_mol.GetMaxAtomIdx() == target_mol.GetMaxAtomIdx()
+    # It's not clear to me that we have to loop over all the atoms in the molecule,
+    # if OpenEye knows they are connected properly, then setting the residue name
+    # and residue number for one atom should be enough, but keeping this loop
+    # seems safe and won't be a bottleneck.
+    for (reference_atom, target_atom) in reference_to_target_mapping.items():
+        reference = reference_mol.GetAtom(OEHasAtomIdx(reference_atom))
+        reference_residue = OEAtomGetResidue(reference)
+        reference_resname = reference_residue.GetName()
+        reference_resnum = reference_residue.GetResidueNumber()
+        # I believe this gets set to 'UNL' if OpenEye can't recognize the residue name.
+        # Thus, I'm adding an override to manually set the residue name, and simply.
+
+        target = target_mol.GetAtom(OEHasAtomIdx(target_atom))
+        target_name = target.GetName()
+        target_residue = OEAtomGetResidue(target)
+        target_resname = target_residue.GetName()
+        target_resnum = target_residue.GetResidueNumber()
+
+        if resname is not None:
+            target_residue.SetName(resname)
+        else:
+            target_residue.SetName(reference_resname)
+        target_residue.SetResidueNumber(reference_resnum)
+
+        print(f'({target_name:4}) {target_resname:4} {target_resnum:4} → '
+              f'{target_residue.GetName():4} {target_residue.GetResidueNumber():4}')
+    return target_mol
+
+
 def remap_type(reference_to_target_mapping, reference_mol, target_mol):
-    print('Remap atom types...')
+    print('Remapping atom types...')
     print('Existing → New')
     assert reference_mol.GetMaxAtomIdx() == target_mol.GetMaxAtomIdx()
     for (reference_atom, target_atom) in reference_to_target_mapping.items():
@@ -134,7 +208,7 @@ def remap_type(reference_to_target_mapping, reference_mol, target_mol):
         reference_type = reference.GetType()
 
         target = target_mol.GetAtom(OEHasAtomIdx(target_atom))
-        # This will be zero, if the target molecule was built from SMILES
+        # This will be None, if the target molecule was built from SMILES
         target_type = target.GetType()
         target_name = target.GetName()
 
@@ -144,7 +218,7 @@ def remap_type(reference_to_target_mapping, reference_mol, target_mol):
 
 
 def remap_coordinates(reference_to_target_mapping, reference_mol, target_mol):
-    print('Remap coordinates...')
+    print('Remapping coordinates...')
     print('Existing → New')
     assert reference_mol.GetMaxAtomIdx() == target_mol.GetMaxAtomIdx()
     mapped_coordinates = np.zeros((reference_mol.GetMaxAtomIdx(), 3))
